08c00a27a2d4fb471378078cae9d11ed5ff94ed3
fierliu <fierliu@126.com>
2023-05-27 21:52:33 +0800

    Update utils.lua

--- lua/clipboard-image/utils.lua
+++ lua/clipboard-image/utils.lua
@@ -113,7 +113,8 @@ M.get_img_path = function(dir, img_name, is_txt)
     -- ./.assets/markdonwfile_name/img_name.png
     return ".assets/" .. file_name_short .. "/"
   end
-
+  
+  dir = dir .. "/" .. file_name_short		
   if this_os == "Windows" and is_txt ~= "txt" then
     dir = M.resolve_dir(dir, "\\")
   else
